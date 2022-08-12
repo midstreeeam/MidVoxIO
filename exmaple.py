24f70ac3f4cee568d7b75f9b12008d763f3bb031
midstreeeam <midstream.lou@gmail.com>
2022-08-12 20:29:36 +0800

    test->example

---
+++ exmaple.py
@@ -0,0 +1,8 @@
+from voxio import Voxio
+
+# Voxio.viz_vox('vox/99/3x3x3.vox')
+# Voxio.viz_vox('vox/98/cat1-0.vox')
+# Voxio.show_chunks('vox/99/cars.vox')
+# Voxio.viz_vox('vox/99/cars.vox',1)
+# Voxio.show_rendering_attributes('vox/99/3x3x3.vox')
+# vox = Voxio.get_vox('vox/99/cars.vox')
--- test.py
+++
@@ -1,4 +0,0 @@
-from voxio import Voxio
-
-Voxio.viz_vox('vox/99/3x3x3.vox')
-# Voxio.viz_vox('vox/98/cat1-0.vox')
