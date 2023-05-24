770fbbda874cf542dbc97d684289946d205e6ebe
midstreeeam <midstream.lou@gmail.com>
2023-05-24 14:20:34 +0800

    release

--- src/examples.py
+++ src/examples.py
@@ -1,14 +1,14 @@
 from pyvox.models import *
 from pyvox.voxio import  * 
 
-# print(vox_to_arr('vox/99/3x3x3.vox'))
-# viz_vox('vox/98/cat.vox')
-# show_chunks('vox/99/cars.vox')
-# viz_vox('vox/99/cars.vox',1)
-# print(get_rendering_attributes('vox/99/3x3x3.vox'))
-# vox = get_vox('vox/99/cars.vox')
+# print(vox_to_arr('assets/vox/99/3x3x3.vox'))
+# viz_vox('assets/vox/98/cat.vox')
+# show_chunks('assets/vox/99/cars.vox')
+# viz_vox('assets/vox/99/cars.vox',1)
+# print(get_rendering_attributes('assets/vox/99/3x3x3.vox'))
+# vox = get_vox('assets/vox/99/cars.vox')
 
-# v=vox_to_arr('vox/98/cat.vox')
+# v=vox_to_arr('assets/vox/98/cat.vox')
 # path='palette/cat.png'
 
 # write_list_to_vox(v,'test.vox',path)
